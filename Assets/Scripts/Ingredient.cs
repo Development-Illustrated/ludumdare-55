7cd9b1c7d3677e9792fc4acd3658de567c2726fd
Adam Probertt <adam.probert@rocketscience.gg>
2024-04-14 12:40:47 +0100

    Yeet to summoning circle!

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -12,6 +12,25 @@ public class Ingredient : MonoBehaviour
 {
     
 
-    [SerializeField]
-    public OrbColor color;
+    [SerializeField] public OrbColor color;
+    [SerializeField] public bool debugMode;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(debugMode){Debug.Log("PlayerController: OnTriggerEnter called with " + other.tag);}
+
+        if (other.tag == "Summonager")
+        {
+            if(debugMode){Debug.Log("Ingredient: OnTriggerEnter called with " + other.tag);}
+            Summonager hitSummonager = other.GetComponent<Summonager>();
+            bool gr8Success = hitSummonager.Deposit(this);
+            if (gr8Success)
+            {
+                this.GetComponent<Rigidbody>().isKinematic = false;
+                this.GetComponent<Animator>().enabled = true;
+            }
+        }
+    }
 }
+
+
