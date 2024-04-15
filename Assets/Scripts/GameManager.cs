09ac6593371713aa30f8668755cf6c064a17ffc7
DanNeenan <daniel_neenan@hotmail.co.uk>
2024-04-15 20:51:31 +0100

    Display end game screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,28 +12,58 @@ public class GameManager : MonoBehaviour
 
     protected int score = 0;
     [SerializeField] protected TMP_Text scoreText;
+    [SerializeField] protected TMP_Text finalScoreText;
     
     [SerializeField] protected int maxFailableRequests = 10;
     protected int failedRequests = 0;
     [SerializeField] protected TMP_Text failedText;
+    [SerializeField] protected GameObject endGameUI;
+    [SerializeField] protected GameObject inGameUI;
 
     [HideInInspector]
     private List<Color> OrbColors = new List<Color>();
+
+    private void ResetGameData()
+    {
+        score = 0;
+        failedRequests = 0;
+        scoreText.text = "Score: 0";
+        finalScoreText.text = "0";
+        failedText.text = "Missed orders: 0/" + maxFailableRequests;
+        inGameUI.SetActive(true);
+        endGameUI.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        ResetGameData();
+
+        SceneManager.LoadScene("FinalGameSceneV1");
+    }
+
+    public void QuitToMain()
+    {
+        ResetGameData();
+
+        SceneManager.LoadScene("MainTitle");
+    }
 
     public void IncrementScore(int scoreValue)
     {
         score += scoreValue;
         scoreText.text = "Score: " + score;
+        finalScoreText.text = scoreText.text;
     }
 
     public void IncrementFailure()
     {
         failedRequests += 1;
-        failedText.text = "Missed orders: " + failedRequests;
+        failedText.text = "Missed orders: " + failedRequests + "/" + maxFailableRequests;
 
         if(failedRequests == maxFailableRequests)
         {
-            // KILL THE GAME
+            endGameUI.SetActive(true);
+            inGameUI.SetActive(false);
         }
     }
 
@@ -59,6 +91,9 @@ public class GameManager : MonoBehaviour
         OrbColors.Add(Color.magenta);
 
         CreateSingleton();
+
+        failedText.text = "Missed orders: 0/" + maxFailableRequests;
+        endGameUI.SetActive(false);
     }
 
     private void OnEnable()
